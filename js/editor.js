91b09b16c78c7849c5b079b4a5cb40fb66e4cf4f
Arron Woods <aw@chartblocks.com>
2014-11-03 13:47:37 +0000

    Make new row columns editable too

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,6 @@
-(function($) {
+(function ($) {
 
-    $.fn.cbDashboardEdit = function() {
+    $.fn.cbDashboardEdit = function () {
         var $dashboard = $(this);
 
         var templates = {
@@ -26,7 +26,7 @@
             var $textarea = $modal.find('textarea.content');
             var originalContent = $raw.html();
 
-            $modal.unbind('hide.bs.modal').on('hide.bs.modal', function(e) {
+            $modal.unbind('hide.bs.modal').on('hide.bs.modal', function (e) {
                 if (originalContent !== $textarea.val()) {
                     var discard = confirm('Discard changes?');
                     if (discard) {
@@ -39,10 +39,10 @@
                 }
             });
 
-            $modal.unbind('show.bs.modal').on('show.bs.modal', function(e) {
+            $modal.unbind('show.bs.modal').on('show.bs.modal', function (e) {
                 $textarea.val(originalContent);
 
-                $modal.find('.save').unbind('click.save').bind('click.save', function() {
+                $modal.find('.save').unbind('click.save').bind('click.save', function () {
                     var newContent = originalContent = $textarea.val();
                     updateCol(col, newContent);
                     $modal.modal('hide');
@@ -67,8 +67,8 @@
         function openRowCreator() {
             var $modal = $('#rowModal');
 
-            $modal.unbind('show.bs.modal').on('show.bs.modal', function(e) {
-                $modal.find('.layout').unbind('click.insert').bind('click.insert', function() {
+            $modal.unbind('show.bs.modal').on('show.bs.modal', function (e) {
+                $modal.find('.layout').unbind('click.insert').bind('click.insert', function () {
                     var $option = $(this);
                     var layout = $option.attr('data-layout');
                     addRow(layout);
@@ -90,7 +90,7 @@
                 var $row = $('<div class="row" />');
                 $row.appendTo($dashboard);
 
-                $(template.shapes).each(function(i, shape) {
+                $(template.shapes).each(function (i, shape) {
                     var $col = $('<div class="col" data-shape="' + shape + '" />')
                             .append('<div class="box" />');
 
@@ -102,15 +102,22 @@
             }
         }
 
-        this.find(".col").each(function() {
-            var $col = $(this);
+        function makeColumnEditable(col) {
+            var $col = $(col);
             $col.addClass('editable');
-            $col.on('dblclick', function() {
+            $col.on('dblclick', function () {
                 openEditor(this);
             });
+        }
+
+        this.find(".col").each(function () {
+            makeColumnEditable(this);
         });
 
         this.on('add', openRowCreator);
+        this.on('append', function (e, col) {
+            makeColumnEditable(col);
+        });
 
         return this;
 
